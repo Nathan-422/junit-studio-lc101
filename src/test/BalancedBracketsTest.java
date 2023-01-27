f1913588491f01a8ffc7b6f09dbee31f3f18f791
Nathan-422 <natiouswright@gmail.com>
2023-01-26 20:14:45 -0600

    Finished studio, but not with 12 unique tests

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,6 +11,45 @@ public class BalancedBracketsTest {
     @Test
     public void emptyTest() {
         assertEquals(true, true);
+    }
+
+    @Test
+    public void isTrueWhenGivenOnePairOfBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void isFalseWhenNotGivenAPairOfBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void isFalseWhenBracketsGivenOutOfOrder() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void isFalseWhenGivenUnevenNumberOfBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[]"));
+    }
+
+    @Test
+    public void isTrueWithBalancedBracketsAndSomeWords() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[Hello[there]general][kenobi]"));
+    }
+    @Test
+    public void isTrueWithNestedBalancedBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[][[[[][]][]][]][]"));
+    }
+
+    @Test
+    public void isTrueWithNoBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Some text"));
+    }
+
+    @Test
+    public void isTrueWhenGivenTwoPairsOfNonNestedBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[][]"));
     }
 
 
